072faf38ad1e3b1c45800aeae09d1d40766714e5
samuelbrehm <samuelbrehm@gmail.com>
2019-02-20 19:15:58 -0300

    Configuração inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import './src/config/ReactotronConfig';
+import './src/config/DevToolsConfig';
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
