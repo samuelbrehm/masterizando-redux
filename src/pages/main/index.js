b468dc298bee6d11b5166da90d205ff503c9951e
samuelbrehm <samuelbrehm@gmail.com>
2019-02-21 15:56:37 -0300

    Configurado os estilos globais

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-// import styles from './styles';
+import styles from './styles';
 
-const Main = () => <View />;
+const Main = () => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+  </View>
+);
+
+Main.navigationOptions = {
+  header: null,
+};
 
 export default Main;
