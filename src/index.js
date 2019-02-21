ccd974276a0e26dedf0fa7a6063c462509254db2
samuelbrehm <samuelbrehm@gmail.com>
2019-02-20 21:43:21 -0300

    Todos conectados pelo redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import { View } from 'react-native';
 
 import store from './store';
+import TodoList from './TodoList';
 
 // import styles from './styles';
 
 const App = () => (
   <Provider store={store}>
-    <View style={{ backgroundColor: '#FFF', flex: 1 }} />
+    <View style={{ backgroundColor: '#FFF', flex: 1 }}>
+      <TodoList />
+    </View>
   </Provider>
 );
 
